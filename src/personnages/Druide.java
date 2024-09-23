fb8fa182313f51670d779abe2305161e96a2f563
Sylvie Ho <hws5127a@ensfsi.onmicrosoft.com>
2024-09-23 18:26:20 +0200

    tp1

---
+++ src/personnages/Druide.java
@@ -0,0 +1,29 @@
+package personnages;
+
+public class Druide {
+	private String nom;
+	private int effetPotionMin;
+	private int effetPotionMax;
+
+	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
+		this.nom = nom;
+		this.effetPotionMin = effetPotionMin;
+		this.effetPotionMax = effetPotionMax;
+		parler("Bonjour, je suis le druide " + nom 
+				+ "et ma potion peut aller d'une force" + effetPotionMin 
+				+ "à " + effetPotionMax + ".");
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+
+	private String prendreParole() {
+		return "Le druide " + nom + " : ";
+	}
+
+}
---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,42 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + " »");
+	}
+
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("asterix", 8);
+		asterix.parler("Je suis créé.");
+		Romain rom = new Romain("rom", 10);
+		rom.parler("Je suis crée");
+		asterix.frapper(rom);
+	}
+}

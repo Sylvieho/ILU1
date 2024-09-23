486a8bc0b9f2bd962f7da33e6280ed976d0b4542
Sylvie Ho <hws5127a@ensfsi.onmicrosoft.com>
2024-09-23 19:51:18 +0200

    village

--- src/lieux/Village.java
+++ src/lieux/Village.java
@@ -1,13 +1,32 @@
 package lieux;
 
+import personnages.Gaulois;
+
 public class Village {
 	private String nom;
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
 
-	public Village(String nom) {
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
+		villageois = new Gaulois[nbVillageoisMaximum] ;
+	}
+	
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois]=gaulois;
+		nbVillageois++;
+	}
+	
+	public Gaulois trouverHabitant(int numero) {
+		return villageois[numero];
 	}
 
 	public String getNom() {
 		return nom;
+	}
+	
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductibles", 30);
+		Gaulois gaulois = village.trouverHabitant(30);
 	}
 }
